ecd71037494c3a10bb0b49809626424f214f3970
Adrien Aury <adrienaury@gmail.com>
2021-08-31 21:47:38 +0200

    Initial commit

---
+++ src/main/java/com/cgi/lino/connector/postgresql/controller/Controller.java
@@ -0,0 +1,65 @@
+package com.cgi.lino.connector.postgresql.controller;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1")
+public class Controller {
+
+	@Autowired
+	private DataSource datasource;
+
+	@GetMapping(path = "/tables", produces = MediaType.APPLICATION_JSON_VALUE)
+	public String getTables(@RequestParam(required = false) String schema) {
+		StringBuilder result = new StringBuilder("{\"version\":\"v1\",\"tables\":[");
+		try {
+			Connection connection = datasource.getConnection();
+			DatabaseMetaData databaseMetaData = connection.getMetaData();
+
+			String tablePrefix = "";
+			ResultSet resultSet = databaseMetaData.getTables(null, null, null, new String[] { "TABLE" });
+			while (resultSet.next()) {
+				String tableName = resultSet.getString("TABLE_NAME");
+				result.append(tablePrefix);
+
+				tablePrefix = ",";
+				result.append("{\"name\":\"");
+				result.append(tableName);
+				result.append("\",\"keys\":[");
+
+				// primary keys
+				String pkPrefix = "";
+				ResultSet pkRs = databaseMetaData.getPrimaryKeys(null, schema, tableName);
+				while (pkRs.next()) {
+					String pkName = pkRs.getString("COLUMN_NAME");
+					result.append(pkPrefix);
+					pkPrefix = ",";
+					result.append("\"");
+					result.append(pkName);
+					result.append("\"");
+				}
+
+				result.append("]}");
+
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		result.append("]}");
+		return result.toString();
+	}
+
+}
---
+++ src/test/java/com/cgi/lino/connector/db2/LinoConnectorDb2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cgi.lino.connector.db2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LinoConnectorDb2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
