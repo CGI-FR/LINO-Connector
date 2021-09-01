5d01f1dec6ebda3a6476499194fdd2baa3a8d864
Adrien Aury <adrienaury@gmail.com>
2021-09-01 22:20:31 +0200

    feat: ajout /relations, /schemas

--- src/main/java/com/cgi/lino/connector/postgresql/controller/Controller.java
+++ src/main/java/com/cgi/lino/connector/postgresql/controller/Controller.java
@@ -14,12 +14,65 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
 @RestController
 @RequestMapping(path = "/api/v1")
 public class Controller {
 
 	@Autowired
 	private DataSource datasource;
+
+	@Autowired
+	private ObjectMapper mapper;
+
+	@GetMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ObjectNode getInfo() throws SQLException {
+		ObjectNode result = mapper.createObjectNode();
+
+		Connection connection = datasource.getConnection();
+		DatabaseMetaData databaseMetaData = connection.getMetaData();
+
+		ObjectNode database = mapper.createObjectNode();
+		database.put("product", databaseMetaData.getDatabaseProductName());
+		database.put("version", databaseMetaData.getDatabaseProductVersion());
+		result.set("database", database);
+
+		ObjectNode driver = mapper.createObjectNode();
+		driver.put("name", databaseMetaData.getDriverName());
+		driver.put("version", databaseMetaData.getDriverVersion());
+		result.set("driver", driver);
+
+		ObjectNode jdbc = mapper.createObjectNode();
+		jdbc.put("version", databaseMetaData.getJDBCMajorVersion() + databaseMetaData.getJDBCMinorVersion());
+		result.set("jdbc", jdbc);
+
+		return result;
+	}
+
+	@GetMapping(path = "/schemas", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ObjectNode getSchemas() throws SQLException {
+		ObjectNode result = mapper.createObjectNode();
+
+		Connection connection = datasource.getConnection();
+		DatabaseMetaData databaseMetaData = connection.getMetaData();
+
+		ArrayNode schemas = mapper.createArrayNode();
+		ResultSet schemasrs = databaseMetaData.getSchemas();
+		while (schemasrs.next()) {
+			String table_schem = schemasrs.getString("TABLE_SCHEM");
+			String table_catalog = schemasrs.getString("TABLE_CATALOG");
+			ObjectNode schema = mapper.createObjectNode();
+			schema.put("name", table_schem);
+			schema.put("catalog", table_catalog);
+			schemas.add(schema);
+		}
+		result.set("schemas", schemas);
+
+		return result;
+	}
 
 	@GetMapping(path = "/tables", produces = MediaType.APPLICATION_JSON_VALUE)
 	public String getTables(@RequestParam(required = false) String schema) throws SQLException {
@@ -29,7 +82,7 @@ public class Controller {
 		DatabaseMetaData databaseMetaData = connection.getMetaData();
 
 		String tablePrefix = "";
-		ResultSet resultSet = databaseMetaData.getTables(null, null, null, new String[] { "TABLE" });
+		ResultSet resultSet = databaseMetaData.getTables(null, schema, null, new String[] { "TABLE" });
 		while (resultSet.next()) {
 			String tableName = resultSet.getString("TABLE_NAME");
 			result.append(tablePrefix);
@@ -53,6 +106,50 @@ public class Controller {
 
 			result.append("]}");
 
+		}
+
+		result.append("]}");
+		return result.toString();
+	}
+
+	@GetMapping(path = "/relations", produces = MediaType.APPLICATION_JSON_VALUE)
+	public String getRelations(@RequestParam(required = false) String schema) throws SQLException {
+		StringBuilder result = new StringBuilder("{\"version\":\"v1\",\"relations\":[");
+
+		Connection connection = datasource.getConnection();
+		DatabaseMetaData databaseMetaData = connection.getMetaData();
+
+		String fkPrefix = "";
+		ResultSet resultSet = databaseMetaData.getTables(null, schema, null, new String[] { "TABLE" });
+		while (resultSet.next()) {
+			String tableName = resultSet.getString("TABLE_NAME");
+
+			ResultSet fkSet = databaseMetaData.getImportedKeys(null, schema, tableName);
+			while (fkSet.next()) {
+				String fkName = fkSet.getString("FK_NAME");
+				result.append(fkPrefix);
+
+				fkPrefix = ",";
+				result.append("{\"name\":\"");
+				result.append(fkName);
+
+				// parent table
+				result.append("\",\"parent\":{\"name\":\"");
+				result.append(fkSet.getString("PKTABLE_NAME"));
+				result.append("\",\"keys\":[\"");
+				result.append(fkSet.getString("PKCOLUMN_NAME"));
+				result.append("\"]");
+
+				// child table
+				result.append("},\"child\":{\"name\":\"");
+				result.append(tableName);
+				result.append("\",\"keys\":[\"");
+				result.append(fkSet.getString("FKCOLUMN_NAME"));
+				result.append("\"]");
+
+				result.append("}}");
+
+			}
 		}
 
 		result.append("]}");
