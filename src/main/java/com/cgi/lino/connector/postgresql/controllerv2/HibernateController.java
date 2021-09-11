dfeb97f423ef60f2c63f7afe1070e0dab1105d53
Adrien Aury <adrienaury@gmail.com>
2021-09-11 15:04:14 +0200

    fix(pulldatav2): where and limit

--- src/main/java/com/cgi/lino/connector/postgresql/controllerv2/HibernateController.java
+++ src/main/java/com/cgi/lino/connector/postgresql/controllerv2/HibernateController.java
@@ -3,6 +3,7 @@ package com.cgi.lino.connector.postgresql.controllerv2;
 import java.io.IOException;
 import java.sql.SQLException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 import java.util.stream.Stream;
 
@@ -38,18 +39,53 @@ public class HibernateController {
 	public HibernateController(final EntityManager entityManager, final ObjectMapper mapper) {
 		this.entityManager = entityManager;
 		this.mapper = mapper;
+
+		mapper.getFactory().disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
 	}
 
 	@GetMapping(path = "/data/{tableName}", produces = MediaType.APPLICATION_NDJSON_VALUE)
 	public ResponseEntity<StreamingResponseBody> pullData(@RequestParam(required = false) String schema,
 			@PathVariable("tableName") String tableName, @RequestBody(required = false) Map<String, Object> filter)
 			throws SQLException, IOException {
+		String where = "where 1=1";
+		int limit = 0;
 
-		mapper.getFactory().disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
+		if (filter != null) {
+			String filterWhere = (String) filter.get("where");
+			if (filterWhere != null && !filterWhere.isBlank()) {
+				where = "where " + filterWhere;
+			}
+
+			@SuppressWarnings("unchecked")
+			Map<String, Object> filterValues = (Map<String, Object>) filter.get("values");
+			for (Iterator<Map.Entry<String, Object>> iterator = filterValues.entrySet().iterator(); iterator
+					.hasNext();) {
+				Map.Entry<String, Object> filterValue = iterator.next();
+				if (filterValue.getValue() instanceof String) {
+					where = where + " and " + filterValue.getKey() + "='" + filterValue.getValue() + "'";
+				} else if (filterValue.getValue() instanceof Integer) {
+					where = where + " and " + filterValue.getKey() + "=" + filterValue.getValue();
+				} else {
+					this.logger.error("Unsupported filter type : " + filterValue.getValue().getClass());
+				}
+			}
+
+			limit = (int) filter.get("limit");
+		}
+
+		final String filterClause;
+		if (limit > 0) {
+			filterClause = where + " limit " + limit;
+		} else {
+			filterClause = where;
+		}
+
+		this.logger.info("Select from " + tableName + " " + filterClause);
 
 		StreamingResponseBody stream = out -> {
 			@SuppressWarnings("unchecked")
-			Stream<Tuple> result = entityManager.createNativeQuery("SELECT * FROM " + tableName, Tuple.class)
+			Stream<Tuple> result = entityManager
+					.createNativeQuery("select * from " + tableName + " " + filterClause, Tuple.class)
 					.getResultStream();
 			result.forEach(entry -> {
 				try {
