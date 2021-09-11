fd922a62323c1ee8615db121dfd0a28b11cbae5c
Adrien Aury <adrienaury@gmail.com>
2021-09-11 15:42:22 +0200

    feat(v2): info, schemas, tables, relations

--- src/main/java/com/cgi/lino/connector/postgresql/controllerv2/HibernateController.java
+++ src/main/java/com/cgi/lino/connector/postgresql/controllerv2/HibernateController.java
@@ -1,6 +1,9 @@
 package com.cgi.lino.connector.postgresql.controllerv2;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -12,6 +15,7 @@ import java.util.stream.Stream;
 import javax.persistence.EntityManager;
 import javax.persistence.Query;
 import javax.persistence.Tuple;
+import javax.sql.DataSource;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -28,6 +32,8 @@ import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBo
 import com.cgi.lino.connector.postgresql.controller.Controller;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 @RestController
 @RequestMapping(path = "/api/v2")
@@ -35,15 +41,154 @@ public class HibernateController {
 
 	private Logger logger = LoggerFactory.getLogger(Controller.class);
 
+	private final DataSource datasource;
+
 	private final EntityManager entityManager;
 
 	private final ObjectMapper mapper;
 
-	public HibernateController(final EntityManager entityManager, final ObjectMapper mapper) {
+	public HibernateController(final DataSource datasource, final EntityManager entityManager,
+			final ObjectMapper mapper) {
+		this.datasource = datasource;
 		this.entityManager = entityManager;
 		this.mapper = mapper;
 
 		mapper.getFactory().disable(JsonGenerator.Feature.AUTO_CLOSE_TARGET);
+	}
+
+	@GetMapping(path = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ObjectNode getInfo() throws SQLException {
+		ObjectNode result = mapper.createObjectNode();
+
+		try (Connection connection = datasource.getConnection()) {
+			DatabaseMetaData databaseMetaData = connection.getMetaData();
+
+			ObjectNode database = mapper.createObjectNode();
+			database.put("product", databaseMetaData.getDatabaseProductName());
+			database.put("version", databaseMetaData.getDatabaseProductVersion());
+			result.set("database", database);
+
+			ObjectNode driver = mapper.createObjectNode();
+			driver.put("name", databaseMetaData.getDriverName());
+			driver.put("version", databaseMetaData.getDriverVersion());
+			result.set("driver", driver);
+
+			ObjectNode jdbc = mapper.createObjectNode();
+			jdbc.put("version", databaseMetaData.getJDBCMajorVersion() + databaseMetaData.getJDBCMinorVersion());
+			result.set("jdbc", jdbc);
+		}
+
+		return result;
+	}
+
+	@GetMapping(path = "/schemas", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ObjectNode getSchemas() throws SQLException {
+		ObjectNode result = mapper.createObjectNode();
+
+		try (Connection connection = datasource.getConnection()) {
+			DatabaseMetaData databaseMetaData = connection.getMetaData();
+
+			ArrayNode schemas = mapper.createArrayNode();
+			try (ResultSet schemasrs = databaseMetaData.getSchemas()) {
+				while (schemasrs.next()) {
+					String table_schem = schemasrs.getString("TABLE_SCHEM");
+					String table_catalog = schemasrs.getString("TABLE_CATALOG");
+					ObjectNode schema = mapper.createObjectNode();
+					schema.put("name", table_schem);
+					schema.put("catalog", table_catalog);
+					schemas.add(schema);
+				}
+			}
+			result.set("schemas", schemas);
+		}
+
+		return result;
+	}
+
+	@GetMapping(path = "/tables", produces = MediaType.APPLICATION_JSON_VALUE)
+	public String getTables(@RequestParam(required = false) String schema) throws SQLException {
+		StringBuilder result = new StringBuilder("{\"version\":\"v1\",\"tables\":[");
+
+		try (Connection connection = datasource.getConnection()) {
+			DatabaseMetaData databaseMetaData = connection.getMetaData();
+
+			String tablePrefix = "";
+			try (ResultSet resultSet = databaseMetaData.getTables(null, schema, null, new String[] { "TABLE" })) {
+				while (resultSet.next()) {
+					String tableName = resultSet.getString("TABLE_NAME");
+					result.append(tablePrefix);
+
+					tablePrefix = ",";
+					result.append("{\"name\":\"");
+					result.append(tableName);
+					result.append("\",\"keys\":[");
+
+					// primary keys
+					String pkPrefix = "";
+					try (ResultSet pkRs = databaseMetaData.getPrimaryKeys(null, schema, tableName)) {
+						while (pkRs.next()) {
+							String pkName = pkRs.getString("COLUMN_NAME");
+							result.append(pkPrefix);
+							pkPrefix = ",";
+							result.append("\"");
+							result.append(pkName);
+							result.append("\"");
+						}
+					}
+
+					result.append("]}");
+				}
+			}
+		}
+
+		result.append("]}");
+		return result.toString();
+	}
+
+	@GetMapping(path = "/relations", produces = MediaType.APPLICATION_JSON_VALUE)
+	public String getRelations(@RequestParam(required = false) String schema) throws SQLException {
+		StringBuilder result = new StringBuilder("{\"version\":\"v1\",\"relations\":[");
+
+		try (Connection connection = datasource.getConnection()) {
+			DatabaseMetaData databaseMetaData = connection.getMetaData();
+
+			String fkPrefix = "";
+			try (ResultSet resultSet = databaseMetaData.getTables(null, schema, null, new String[] { "TABLE" })) {
+				while (resultSet.next()) {
+					String tableName = resultSet.getString("TABLE_NAME");
+
+					try (ResultSet fkSet = databaseMetaData.getImportedKeys(null, schema, tableName)) {
+						while (fkSet.next()) {
+							String fkName = fkSet.getString("FK_NAME");
+							result.append(fkPrefix);
+
+							fkPrefix = ",";
+							result.append("{\"name\":\"");
+							result.append(fkName);
+
+							// parent table
+							result.append("\",\"parent\":{\"name\":\"");
+							result.append(fkSet.getString("PKTABLE_NAME"));
+							result.append("\",\"keys\":[\"");
+							result.append(fkSet.getString("PKCOLUMN_NAME"));
+							result.append("\"]");
+
+							// child table
+							result.append("},\"child\":{\"name\":\"");
+							result.append(tableName);
+							result.append("\",\"keys\":[\"");
+							result.append(fkSet.getString("FKCOLUMN_NAME"));
+							result.append("\"]");
+
+							result.append("}}");
+						}
+					}
+				}
+			}
+		}
+
+		result.append("]}");
+		return result.toString();
 	}
 
 	@GetMapping(path = "/data/{tableName}", produces = MediaType.APPLICATION_NDJSON_VALUE)
