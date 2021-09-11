158389347bac8b7993b7e1fdb942ac0b1748f79d
Adrien Aury <adrienaury@gmail.com>
2021-09-11 18:21:09 +0200

    fix: use dialect postgresql 10

--- src/main/java/com/cgi/lino/connector/postgresql/controllerv2/PostgreSQLCustomDialect.java
+++ src/main/java/com/cgi/lino/connector/postgresql/controllerv2/PostgreSQLCustomDialect.java
@@ -2,13 +2,13 @@ package com.cgi.lino.connector.postgresql.controllerv2;
 
 import java.sql.Types;
 
-import org.hibernate.dialect.PostgreSQL94Dialect;
+import org.hibernate.dialect.PostgreSQL10Dialect;
 
 import com.vladmihalcea.hibernate.type.array.StringArrayType;
 
-public class PostgreSQL94CustomDialect extends PostgreSQL94Dialect {
+public class PostgreSQLCustomDialect extends PostgreSQL10Dialect {
 
-	public PostgreSQL94CustomDialect() {
+	public PostgreSQLCustomDialect() {
 		super();
 		// Works with the sakila schema
 		// https://github.com/fspacek/docker-postgres-sakila/blob/master/step_1.sql
